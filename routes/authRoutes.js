c359ce3f6a5a03a45def4a095603da2236ad8128
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-08 17:11:27 +0545

    Refactoring Auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 
 // Load COnstructor
 import registerController from "../controllers/authController.js";
-
+router.post("/register", registerController);
 export default router;
