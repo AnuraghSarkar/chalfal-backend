2fe001719581b686369b869e02ff31fcb93570df
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-12 20:07:24 +0545

    dumped fake data in mongo

--- models/post.js
+++ models/post.js
@@ -3,11 +3,11 @@ import Mongoose from "mongoose";
 const { Schema } = Mongoose;
 
 // Post and COmment Schema
-const CommentSchema = new Schema({
+const PostSchema = new Schema({
   author: { type: String, required: true },
   title: { type: String },
   body: { type: String, required: true },
   postedAt: { type: Date, default: Date.now },
 });
 
-export default Mongoose.model("Comment", CommentSchema);
+export default Mongoose.model("Post", PostSchema);
--- routes/api/post.js
+++ routes/api/post.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
+import Post from "../../models/post";
 import dotenv from "dotenv";
 dotenv.config();
 
-router.get("/posts", (req, res) => { 
-    res.send("Hello World");
+router.get("/posts", (req, res) => {
+  Post.find()
+    .then((posts) => res.json(posts))
+    .catch((err) => res.status(400).json(err));
 });
-
 export default router;
