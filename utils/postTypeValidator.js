96f159f9dcfc4b223643dddcb74f4f2ed669c9a7
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-13 18:14:13 +0545

    validated image type post

--- utils/postTypeValidator.js
+++ utils/postTypeValidator.js
@@ -1,5 +1,6 @@
 const validator = require('validator');
 
+// Validates the text submission
 const parseTextSubmission = (textSubmission) => {
   if (!textSubmission) {
     throw new Error("Text is required for text post type");
@@ -7,6 +8,13 @@ const parseTextSubmission = (textSubmission) => {
   return textSubmission;
 };
 
+// Validates the image submission
+const parseImageSubmission = (imageSubmission) => { 
+    if (!imageSubmission) {
+        throw new Error("Image is required for image post type");
+    }
+    return imageSubmission;
+};
 
 const postTypeValidator = (type, text, link, image) => {
     switch (type) { 
@@ -15,6 +23,11 @@ const postTypeValidator = (type, text, link, image) => {
               postType: "text",
               textSubmission: parseTextSubmission(text),
             };
+        case 'Image':
+            return {
+                postType: "image",
+                imageSubmission: parseImageSubmission(image),
+            }
         default:
             throw new Error("Invalid post type please include a valid post type");
     }
