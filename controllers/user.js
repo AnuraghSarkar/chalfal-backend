7f39502e84e83bcac20e476449f3d1634a1eef48
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-13 17:50:52 +0545

    manupalting posts using mongo

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const Post = require("../models/post");
+const { cloudinary, UPLOAD_PRESET } = require("../utils/config");
 
+const paginateResults = require("../utils/paginateResults");
 
 const getUser = async (req, res) => {
   const { username } = req.params;
@@ -11,10 +13,18 @@ const getUser = async (req, res) => {
   const user = await User.findOne({
     username: { $regex: new RegExp("^" + username + "$", "i") },
   });
-    // If user not found
-    if (!user) { 
-        return res.status(404).send({ message: "User not found" });
-    }
+  // If user not found
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  const postsCount = await Post.countDocuments({ author: user._id });
+  const paginatedResults = await paginateResults({ page, limit, postsCount });
+  const posts = await Post.find({ author: user._id })
+    .sort({ createdAt: -1 })
+    .select("-comments")
+    .limit(limit)
+    .skip(paginatedResults.startIndex)
+    .populate("author", "username");
 };
 
-module.exports = {getUser};
+module.exports = { getUser };
