1c63ddad95dc7ca4767d18673db59a5613782e65
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-13 18:12:45 +0545

    validated text type post

--- utils/postTypeValidator.js
+++ utils/postTypeValidator.js
@@ -1,6 +1,22 @@
 const validator = require('validator');
 
-const postTypeValidator = (type, text, link, image) => {
+const parseTextSubmission = (textSubmission) => {
+  if (!textSubmission) {
+    throw new Error("Text is required for text post type");
+  }
+  return textSubmission;
+};
 
+
+const postTypeValidator = (type, text, link, image) => {
+    switch (type) { 
+        case 'Text':
+            return {
+              postType: "text",
+              textSubmission: parseTextSubmission(text),
+            };
+        default:
+            throw new Error("Invalid post type please include a valid post type");
+    }
 };
  module.exports = postTypeValidator;
