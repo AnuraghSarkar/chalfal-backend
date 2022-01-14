8bc8d65d92d0d8c265ca7e721dcc20366d199dd2
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 15:46:39 +0545

    working in votes in post contoller

---
+++ controllers/postVote.js
@@ -0,0 +1,7 @@
+const Post = require("../models/post");
+const User = require("../models/user");
+const pointsCalculator = require("../utils/pointsCalculator");
+
+const upvotePost = async (req, res) => { };
+
+module.exports = upvotePost;
