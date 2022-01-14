710e6e56d85cfe2385979ad80019fcdf020a3d46
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 15:03:52 +0545

    controller for getting subreddits created

--- controllers/subreddit.js
+++ controllers/subreddit.js
@@ -2,18 +2,11 @@ const Subreddit = require('../models/subreddit');
 const Post = require('../models/post');
 const User = require('../models/user');
 
+// fetching all subreddits
 const getSubreddits = (req, res) => {
-  Subreddit.findOne({ name: req.params.subreddit })
-    .then(subreddit => {
-      if (!subreddit) {
-        res.status(404).json({ message: 'Subreddit not found' });
-      } else {
-        res.status(200).json(subreddit);
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Internal server error' });
-    });
+    const allSubreddits = await Subreddit.find({}).select('id subredditName');
+    res.status(200).json(allSubreddits);
 }
+
 
 module.exports = {getSubreddits};
