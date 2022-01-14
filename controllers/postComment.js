16eb07aff94324647f03bc4d3ddbc7314106fc48
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 14:36:35 +0545

    added delete comment controller for the post

--- controllers/postComment.js
+++ controllers/postComment.js
@@ -45,6 +45,37 @@ const postComment = async (req, res) => {
     res.status(201).json({addedComment});
 };
 
-const deleteComment = async (req, res) => { };
+const deleteComment = async (req, res) => { 
+    const { id, commentId } = req.params;
+    // fetching post
+    const post = await Post.findById(id);
+    // fetching user
+    const user = await User.findById(req.user);
+    // Checking if post exists
+    if (!post) { 
+        return res.status(404).send({ message: `Post with ID:${id} not found.` });
+    }
+    // Checking if user exists
+    if (!user) { 
+        return res.status(404).send({ message: `User not found.` });
+    }
+    // Targeting comment
+    const targetComment = post.comments.find(comment => comment._id.toString() === commentId);
+    // Checking if comment exists
+    if (!targetComment) {
+        return res.status(404).send({ message: `Comment with ID:${commentId} not found.` });
+    }
+    // Checking if user is the owner of the comment
+    if (targetComment.commentedBy.toString() !== user._id.toString()) {
+        return res.status(401).send({ message: `You are not the owner of the comment.` });
+    }
+    // Removing comment
+    post.comments = post.comments.filter(comment => comment._id.toString() !== commentId);
+    // Counting number of comments
+    post.commentCount = numOfComments(post.comments);
+    // Saving post
+    await post.save();
+    res.status(204).json({message: `Comment with ID:${commentId} deleted.`}).end();
+};
 
 module.exports = { postComment, deleteComment };
