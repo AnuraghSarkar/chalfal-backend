0c171b5573eb65c5ba634c2bd3af2554f78ea7c3
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 16:32:29 +0545

    created routers for user controller

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,15 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const { auth } = require("../utils/middleware");
 
+const {
+  getUser,
+  setuserAvatar,
+  removeUserAvatar,
+} = require("../controllers/user");
+
+const router = express.Router();
+router.get("/:username", getUser);
+router.post('/avatar', auth, setuserAvatar);
+router.delete('/avatar', auth, removeUserAvatar);
 
 module.exports = router;
