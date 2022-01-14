bca508870cadac3f9d403fce40d042ef8fb7f709
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 15:58:16 +0545

    created post downvote controller

--- controllers/postVote.js
+++ controllers/postVote.js
@@ -49,6 +49,51 @@ const upvotePost = async (req, res) => {
   res.status(201).end();
 };
 // downvote post
-const downvotePost = async (req, res) => { };
+const downvotePost = async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findById(id);
+  const user = await User.findById(req.user);
+  // checking if user is empty
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  // checking if post is empty
+  if (!post) {
+    return res.status(404).send({ message: `Post with id ${id} not found` });
+  }
+  // checking if user is the owner of the post
+  const author = await User.findById(post.author);
+  if (!author) {
+    return res.status(404).send({ message: "Author user not found" });
+  }
+  if (post.downvotedBy.includes(user._id.toString())) {
+    post.downvotedBy = post.downvotedBy.filter(
+      (auth) => auth.toString() !== user._id.toString()
+    );
+  } else {
+    post.downvotedBy = post.downvotedBy.concat(user._id);
+    post.upvotedBy = post.upvotedBy.filter(
+      (auth) => auth.toString() !== user._id.toString()
+    );
+    author.karmaPoints.postKarma--;
+  }
+  // calcualting points
+  const calculatedData = pointsCalculator(
+    post.upvotedBy.length,
+    post.downvotedBy.length,
+    post.createdAt
+  );
+  post.pointsCount = calculatedData.pointsCount;
+  post.voteRatio = calculatedData.voteRatio;
+  post.hotAlgorithm = calculatedData.hotAlgorithm;
+  post.controversialAlgorithm = calculatedData.controversialAlgorithm;
 
-module.exports = {upvotePost, downvotePost};
+  // saving post
+  await post.save();
+  await author.save();
+
+  // sending response
+  res.status(201).end();
+};
+
+module.exports = { upvotePost, downvotePost };
