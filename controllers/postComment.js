39da1da03254e7cc8c7df9ab2084baf5e6904d22
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 14:42:32 +0545

    created controller for updating the comment in post

--- controllers/postComment.js
+++ controllers/postComment.js
@@ -31,54 +31,111 @@ const postComment = async (req, res) => {
     upvotedBy: [user._id],
     pointsCount: 1,
   });
-    // Counting number of comments
-    post.commentCount = numOfComments(post.comments);
-    // Saving post\
-    const savedPost = await post.save();
-    // Populating post with user
-    const populatedPost = await savedPost.populate("comments.commentedBy", 'username').execPopulate();
-    // Couting karma
-    user.karmaPoints.commentKarma++;
-    await user.save();
-    // Sending response
-    const addedComment = populatedPost.comments[savedPost.comments.length - 1];
-    res.status(201).json({addedComment});
+  // Counting number of comments
+  post.commentCount = numOfComments(post.comments);
+  // Saving post\
+  const savedPost = await post.save();
+  // Populating post with user
+  const populatedPost = await savedPost
+    .populate("comments.commentedBy", "username")
+    .execPopulate();
+  // Couting karma
+  user.karmaPoints.commentKarma++;
+  await user.save();
+  // Sending response
+  const addedComment = populatedPost.comments[savedPost.comments.length - 1];
+  res.status(201).json({ addedComment });
 };
 
-const deleteComment = async (req, res) => { 
-    const { id, commentId } = req.params;
-    // fetching post
-    const post = await Post.findById(id);
-    // fetching user
-    const user = await User.findById(req.user);
-    // Checking if post exists
-    if (!post) { 
-        return res.status(404).send({ message: `Post with ID:${id} not found.` });
-    }
-    // Checking if user exists
-    if (!user) { 
-        return res.status(404).send({ message: `User not found.` });
-    }
-    // Targeting comment
-    const targetComment = post.comments.find(comment => comment._id.toString() === commentId);
-    // Checking if comment exists
-    if (!targetComment) {
-        return res.status(404).send({ message: `Comment with ID:${commentId} not found.` });
-    }
-    // Checking if user is the owner of the comment
-    if (targetComment.commentedBy.toString() !== user._id.toString()) {
-        return res.status(401).send({ message: `You are not the owner of the comment.` });
-    }
-    // Removing comment
-    post.comments = post.comments.filter(comment => comment._id.toString() !== commentId);
-    // Counting number of comments
-    post.commentCount = numOfComments(post.comments);
-    // Saving post
-    await post.save();
-    res.status(204).json({message: `Comment with ID:${commentId} deleted.`}).end();
+const deleteComment = async (req, res) => {
+  const { id, commentId } = req.params;
+  // fetching post
+  const post = await Post.findById(id);
+  // fetching user
+  const user = await User.findById(req.user);
+  // Checking if post exists
+  if (!post) {
+    return res.status(404).send({ message: `Post with ID:${id} not found.` });
+  }
+  // Checking if user exists
+  if (!user) {
+    return res.status(404).send({ message: `User not found.` });
+  }
+  // Targeting comment
+  const targetComment = post.comments.find(
+    (comment) => comment._id.toString() === commentId
+  );
+  // Checking if comment exists
+  if (!targetComment) {
+    return res
+      .status(404)
+      .send({ message: `Comment with ID:${commentId} not found.` });
+  }
+  // Checking if user is the owner of the comment
+  if (targetComment.commentedBy.toString() !== user._id.toString()) {
+    return res
+      .status(401)
+      .send({ message: `You are not the owner of the comment.` });
+  }
+  // Removing comment
+  post.comments = post.comments.filter(
+    (comment) => comment._id.toString() !== commentId
+  );
+  // Counting number of comments
+  post.commentCount = numOfComments(post.comments);
+  // Saving post
+  await post.save();
+  res
+    .status(204)
+    .json({ message: `Comment with ID:${commentId} deleted.` })
+    .end();
 };
 
 // updating comment
-const updateComment = async (req, res) => { };
+const updateComment = async (req, res) => {
+  const { id, commentId } = req.params;
+  const { comment } = req.body;
+  // validating comment
+  if (!comment) {
+    return res.status(400).send({ message: `Comment can't be empty.` });
+  }
+  // fetching post
+  const post = await Post.findById(id);
+  // fetching user
+  const user = await User.findById(req.user);
+  // Checking if post exists
+  if (!post) {
+    return res.status(404).send({ message: `Post with ID:${id} not found.` });
+  }
+  // Checking if user exists
+  if (!user) {
+    return res.status(404).send({ message: `User not found.` });
+  }
+  // Targeting comment
+  const targetComment = post.comments.find(
+    (comment) => comment._id.toString() === commentId
+  );
+  // Checking if comment exists
+  if (!targetComment) {
+    return res
+      .status(404)
+      .send({ message: `Comment with ID:${commentId} not found.` });
+  }
+  // Checking if user is the owner of the comment
+  if (targetComment.commentedBy.toString() !== user._id.toString()) {
+    return res
+      .status(401)
+      .send({ message: `You are not the owner of the comment.` });
+    }
+    // Updating comment
+    targetComment.commentBody = comment;
+    targetComment.updatedAt = Date.now();
+    // Saving post
+    post.comments = post.comments.map((comment) => { 
+        comment._id.toString() !== commentId ? comment : targetComment
+    });
+    await post.save();
+    res.status(202).json({ message: `Comment updated.` }).end();
+};
 
 module.exports = { postComment, deleteComment, updateComment };
