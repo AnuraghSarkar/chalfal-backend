9c9e868a7521d4e1dd410de47e90ec75c3efc353
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 13:58:18 +0545

    working on controller for comments

---
+++ controllers/postComment.js
@@ -0,0 +1,7 @@
+const Post = require('../models/post');
+const User = require('../models/user');
+const numOfComments = require('../models/numOfComments');
+
+const postComment = async (req, res) => { };
+
+module.exports = {postComment};
