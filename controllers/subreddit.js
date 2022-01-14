4452df74a739548058111f92825758e0cca3075d
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 15:11:06 +0545

    switch statement for sorting post created

--- controllers/subreddit.js
+++ controllers/subreddit.js
@@ -2,6 +2,7 @@ const Subreddit = require("../models/subreddit");
 const Post = require("../models/post");
 const User = require("../models/user");
 
+const paginateResults = require("../utils/paginateResults");
 // fetching all subreddits
 const getSubreddits = (req, res) => {
   const allSubreddits = await Subreddit.find({}).select("id subredditName");
@@ -9,6 +10,38 @@ const getSubreddits = (req, res) => {
 };
 
 // getting all posts from a subreddit
-const getSubredditPosts = async (req, res) => {};
+const getSubredditPosts = async (req, res) => {
+    const { subredditName } = req.params;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const sortBy = req.query.sortBy || "createdAt";
+
+    // sorting subreddit posts
+    let sortQuery;
+    // switch statement to sort by different fields
+    switch (sortBy) { 
+        case 'new':
+            sortQuery = { createdAt: -1 };
+            break;
+        case 'top':
+            sortQuery = { pointsCount: -1 };
+            break;
+        case 'hot':
+            sortQuery = { hotAlgorithm: -1 };
+            break;
+        case 'best':
+            sortQuery = { voteRation: -1 };
+            break;
+        case 'controversial':
+            sortQuery = { controversialAlgorithm: -1 };
+            break;
+        case 'old':
+            sortQuery = { createdAt: 1 };
+            break;
+        default:
+            sortQuery = { createdAt: -1 };
+
+    }
+};
 
 module.exports = { getSubreddits, getSubredditPosts };
