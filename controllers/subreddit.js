5c62a2199257123c17d5bfd23a5091e0822ef4c3
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 17:14:27 +0545

    optimizing subreddits controlelr

--- controllers/subreddit.js
+++ controllers/subreddit.js
@@ -1,24 +1,21 @@
 const Subreddit = require("../models/subreddit");
-const Post = require("../models/post");
 const User = require("../models/user");
+const Post = require("../models/post");
 
 const paginateResults = require("../utils/paginateResults");
-// fetching all subreddits
-const getSubreddits = (req, res) => {
-  const allSubreddits = Subreddit.find({}).select("id subredditName");
+
+const getSubreddits = async (_req, res) => {
+  const allSubreddits = await Subreddit.find({}).select("id subredditName");
   res.status(200).json(allSubreddits);
 };
 
-// getting all posts from a subreddit
 const getSubredditPosts = async (req, res) => {
   const { subredditName } = req.params;
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const sortBy = req.query.sortBy || "createdAt";
+  const page = Number(req.query.page);
+  const limit = Number(req.query.limit);
+  const sortBy = req.query.sortby;
 
-  // sorting subreddit posts
   let sortQuery;
-  // switch statement to sort by different fields
   switch (sortBy) {
     case "new":
       sortQuery = { createdAt: -1 };
@@ -26,50 +23,51 @@ const getSubredditPosts = async (req, res) => {
     case "top":
       sortQuery = { pointsCount: -1 };
       break;
-    case "hot":
-      sortQuery = { hotAlgorithm: -1 };
-      break;
     case "best":
-      sortQuery = { voteRation: -1 };
+      sortQuery = { voteRatio: -1 };
+      break;
+    case "hot":
+      sortQuery = { hotAlgo: -1 };
       break;
     case "controversial":
-      sortQuery = { controversialAlgorithm: -1 };
+      sortQuery = { controversialAlgo: -1 };
       break;
     case "old":
       sortQuery = { createdAt: 1 };
       break;
     default:
-      sortQuery = { createdAt: -1 };
+      sortQuery = {};
   }
-  // finding subreddit
+
   const subreddit = await Subreddit.findOne({
     subredditName: { $regex: new RegExp("^" + subredditName + "$", "i") },
   }).populate("admin", "username");
-  // if subreddit is not found
+
   if (!subreddit) {
     return res.status(404).send({
-      message: `Subreddit ${subredditName} not found`,
+      message: `Subreddit '${subredditName}' does not exist on server.`,
     });
   }
-  // finding posts
-  const postsCount = await Post.countDocuments({ subreddit: subreddit._id });
-  // paginating posts
+
+  const postsCount = await Post.find({
+    subreddit: subreddit.id,
+  }).countDocuments();
+
   const paginated = paginateResults(page, limit, postsCount);
-  const subredditPosts = await Post.find({ subreddit: subreddit._id })
+  const subredditPosts = await Post.find({ subreddit: subreddit.id })
     .sort(sortQuery)
-    .select("comments")
-    .skip(paginated.startIndex)
+    .select("-comments")
     .limit(limit)
+    .skip(paginated.startIndex)
     .populate("author", "username")
     .populate("subreddit", "subredditName");
 
-  // giving paginated results
   const paginatedPosts = {
     previous: paginated.results.previous,
-    next: paginated.results.next,
     results: subredditPosts,
+    next: paginated.results.next,
   };
-  // sending response
+
   res.status(200).json({ subDetails: subreddit, posts: paginatedPosts });
 };
 
@@ -82,114 +80,83 @@ const getTopSubreddits = async (_req, res) => {
   res.status(200).json(top10Subreddits);
 };
 
-// creating a new subreddit
 const createNewSubreddit = async (req, res) => {
   const { subredditName, description } = req.body;
+
   const admin = await User.findById(req.user);
-  // checking if user already exists
   if (!admin) {
-    return res.status(404).send({ message: "User not found" });
+    return res
+      .status(404)
+      .send({ message: "User does not exist in database." });
   }
-  // checking if subreddit already exists
+
   const existingSubName = await Subreddit.findOne({
     subredditName: { $regex: new RegExp("^" + subredditName + "$", "i") },
   });
-  // if subreddit exists
+
   if (existingSubName) {
-    return res.status(400).send({
-      message: `Subreddit named ${subredditName} already exists. Please choose another name.`,
+    return res.status(403).send({
+      message: `Subreddit having same name "${subredditName}" already exists. Choose another name.`,
     });
   }
-  // creating new subreddit
+
   const newSubreddit = new Subreddit({
     subredditName,
     description,
     admin: admin._id,
-    suscribedBy: [admin._id],
+    subscribedBy: [admin._id],
     subscriberCount: 1,
   });
-  // saving new subreddit
+
   const savedSubreddit = await newSubreddit.save();
-  // adding admin as a suscribed user
+
   admin.subscribedSubs = admin.subscribedSubs.concat(savedSubreddit._id);
   await admin.save();
-  // sending response
-  res.status(201).json(savedSubreddit);
+
+  return res.status(201).json(savedSubreddit);
 };
-// editing a subreddit
+
 const editSubDescription = async (req, res) => {
   const { description } = req.body;
   const { id } = req.params;
-  // checking if description is empty
+
   if (!description) {
-    return res.status(400).send({ message: "Description cannot be empty" });
-  }
-  // finding subreddit and admin
-  const subreddit = await Subreddit.findById(id);
-  const admin = await User.findById(req.user);
-  // checking if admin is empty
-  if (!admin) {
-    return res.status(404).send({ message: "User not found" });
-  }
-  // checking if subreddit is empty
-  if (!subreddit) {
     return res
-      .status(404)
-      .send({ message: `Subreddit with id ${id} not found` });
+      .status(400)
+      .send({ message: `Description body can't be empty.` });
   }
 
-  // checking if admin is the owner of the subreddit
-  if (subreddit.admin.toString() !== admin._id.toString()) {
+  const admin = await User.findById(req.user);
+  const subreddit = await Subreddit.findById(id);
+
+  if (!admin) {
     return res
-      .status(403)
-      .send({ message: "You are not the owner of this subreddit" });
+      .status(404)
+      .send({ message: "User does not exist in database." });
   }
-  // updating subreddit
+
+  if (!subreddit) {
+    return res.status(404).send({
+      message: `Subreddit with ID: ${id} does not exist in database.`,
+    });
+  }
+
+  if (subreddit.admin.toString() !== admin._id.toString()) {
+    return res.status(401).send({ message: "Access is denied." });
+  }
+
   subreddit.description = description;
+
   await subreddit.save();
-
-  // sending response
-  res.status(200).end();
+  res.status(202).end();
 };
 
-// deleting a subreddit
-const deleteSubreddit = async (req, res) => {
-  const { id } = req.params;
-  // finding subreddit and admin
-  const subreddit = await Subreddit.findById(id);
-  const admin = await User.findById(req.user);
-  // checking if admin is empty
-  if (!admin) {
-    return res.status(404).send({ message: "User not found" });
-  }
-  // checking if subreddit is empty
-  if (!subreddit) {
-    return res
-      .status(404)
-      .send({ message: `Subreddit with id ${id} not found` });
-  }
-  // checking if admin is the owner of the subreddit
-  if (subreddit.admin.toString() !== admin._id.toString()) {
-    return res
-      .status(403)
-      .send({ message: "You are not the owner of this subreddit" });
-  }
-  // deleting subreddit
-  await subreddit.remove();
-  // sending response
-  res.status(200).end();
-};
-
-// being  subscribers
 const subscribeToSubreddit = async (req, res) => {
   const { id } = req.params;
+
   const subreddit = await Subreddit.findById(id);
   const user = await User.findById(req.user);
-  // checking if user is empty
-  if (!user) {
-    return res.status(404).send({ message: "User not found" });
-  }
-  // checking if subreddit is already subscribed
+
   if (subreddit.subscribedBy.includes(user._id.toString())) {
     subreddit.subscribedBy = subreddit.subscribedBy.filter(
       (s) => s.toString() !== user._id.toString()
@@ -211,12 +178,12 @@ const subscribeToSubreddit = async (req, res) => {
 
   res.status(201).end();
 };
+
 module.exports = {
   getSubreddits,
   getSubredditPosts,
   getTopSubreddits,
   createNewSubreddit,
   editSubDescription,
-  deleteSubreddit,
   subscribeToSubreddit,
 };
