de0d9884beba1f14211d799a9e903a21e1dd2d0e
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 16:39:43 +0545

    importing required moduels for post routing

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,30 @@
 const express = require("express");
+const { auth } = require("../utils/middleware");
+const {
+  getPosts,
+  getSubscribedPosts,
+  getSearchedPosts,
+  getPostAndComments,
+  createNewPost,
+  updatePost,
+  deletePost,
+} = require("../controllers/post");
+const { upvotePost, downvotePost } = require("../controllers/postVote");
+const {
+  postComment,
+  deleteComment,
+  updateComment,
+  postReply,
+  deleteReply,
+  updateReply,
+} = require("../controllers/postComment");
+const {
+  upvoteComment,
+  downvoteComment,
+  upvoteReply,
+  downvoteReply,
+} = require("../controllers/commentVote");
+
 const router = express.Router();
 
 module.exports = router;
