776512318efca5234276f0b47a520ae9ab0f77c2
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-14 16:15:39 +0545

    validating if comment is already upvoted

--- controllers/commentVote.js
+++ controllers/commentVote.js
@@ -1,6 +1,45 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
-const upvoteComment = async (req, res) => {};
+const upvoteComment = async (req, res) => {
+  const { id, commentId } = req.params;
+  const post = await Post.findById(id);
+  const user = await User.findById(req.user);
+  // checking if user is empty
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  // checking if post is empty
+  if (!post) {
+    return res.status(404).send({ message: `Post with id ${id} not found` });
+  }
+  // targetting comment
+  const targetComment = post.comments.find(
+    (comment) => comment._id.toString() === commentId.toString()
+  );
+  // checking if comment is empty
+  if (!targetComment) {
+    return res
+      .status(404)
+      .send({ message: `Comment with id ${commentId} not found` });
+  }
+  // checking if user is the comment owner
+  const commentAuthor = await User.findById(targetComment.commentedBy);
+  if (!commentAuthor) {
+    return res.status(404).send({ message: "Comment Author not found" });
+  }
+  if (targetComment.upvotedBy.includes(user._id.toSring())) {
+    targetComment.upvotedBy = targetComment.upvotedBy.filter(
+      (upvotedBy) => upvotedBy.toString() !== user._id.toString()
+    );
+    commentAuthor.karmaPoints.commentKarma--;
+  } else {
+    targetComment.upvotedBy = targetComment.upvotedBy.concat(user._id);
+    targetComment.downvotedBy = targetComment.downvotedBy.filter(
+      (d) => d.toString() !== user._id.toString()
+    );
+    commentAuthor.karmaPoints.commentKarma++;
+  }
+};
 
 module.exports = { upvoteComment };
