5387913bcfc618fcd66e7605ae6f873f54048173
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-02-11 14:52:08 +0545

    :fire: heroku port defiend

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const app = require('./app');
-const http = require('http');
-const { PORT } = require('./utils/config');
-const connectToDB = require('./db');
+const app = require("./app");
+const http = require("http");
+const { PORT } = require("./utils/config");
+const connectToDB = require("./db");
 
 connectToDB();
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+server.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
